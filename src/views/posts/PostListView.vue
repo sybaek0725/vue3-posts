747c3d926e4fab0a660409ef0a8897c1c31a20f5
sybaek0725 <sybaek0725@gmail.com>
2024-06-12 23:28:46 +0900

    npm run lint

--- src/views/posts/PostListView.vue
+++ src/views/posts/PostListView.vue
@@ -1,17 +1,17 @@
 <template>
-    <div>
-        <h2>게시글 목록</h2>
-        <hr class="my-4" />
-        <div class="row g-3">
-            <div class="col-4">
-                <PostItem title="제목" content="내용" created-at="2024-06-12" />
-            </div>
-        </div>
-    </div>
+	<div>
+		<h2>게시글 목록</h2>
+		<hr class="my-4" />
+		<div class="row g-3">
+			<div class="col-4">
+				<PostItem title="제목" content="내용" created-at="2024-06-12" />
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
-import PostItem from '@/components/posts/PostItem.vue'
+import PostItem from '@/components/posts/PostItem.vue';
 </script>
 
 <style lang="scss" scoped></style>
